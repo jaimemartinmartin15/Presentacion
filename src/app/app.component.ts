7181dd1955aa070ac7221d4b855bf15e0601bf30
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2025-02-26 19:52:31 +0100

    Mobile view implementation.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,13 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { RestoreScrollService, SeoService } from '@jaimemartinmartin15/jei-devkit-angular-shared';
+import { BurgerSvgComponent } from './svg-icons/burger.component';
 import { JameLogoSvgComponent } from './svg-icons/jame-logo.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink, JameLogoSvgComponent],
+  imports: [CommonModule, RouterOutlet, RouterLink, JameLogoSvgComponent, BurgerSvgComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
@@ -15,5 +16,9 @@ export class AppComponent {
   public constructor(readonly seoService: SeoService, readonly restoreScrollService: RestoreScrollService) {
     seoService.listenNavigationEvents();
     restoreScrollService.listenScrollRestoration();
+  }
+
+  public toggleMenu(divEl: HTMLDivElement) {
+    divEl.classList.toggle('collapsible-links--collapsed')
   }
 }
